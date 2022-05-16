d2250968dbd0235a19db1c7ec327b8fe09b3bfb5
hellozo0 <ahju0011@gmail.com>
2022-05-16 18:59:01 +0900

    [ADD] 파일 세팅

---
+++ week01/src/main/java/com/example/week01/prac/Prac.java
@@ -0,0 +1,4 @@
+package com.example.week01.prac;
+
+public class Prac {
+}
---
+++ week01/src/test/java/com/example/week01/Week01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
