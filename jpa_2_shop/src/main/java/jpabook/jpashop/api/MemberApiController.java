27536f9e274449fb3d370e3ad6a954cdeeaffa4f
An HyeonJu <ahju0011@gmail.com>
2022-07-25 14:17:01 +0900

    [CHORE] 회원 조회 API

--- jpa_2_shop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpa_2_shop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +20,7 @@ public class MemberApiController {
      * 문제점
      * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
      * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
-     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를
-     위한 모든 요청 요구사항을 담기는 어렵다.
+     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 모든 요청 요구사항을 담기는 어렵다.
      * - 엔티티가 변경되면 API 스펙이 변한다.
      * 결론
      * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다. */
@@ -52,6 +53,37 @@ public class MemberApiController {
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
+
+    //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api가 이거 하나인가! 화면에 종속적이지 마라!
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     *조회 V2: 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+    */
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers(); //엔티티 -> DTO 변환
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
     @Data
     static class UpdateMemberRequest {
         private String name;
