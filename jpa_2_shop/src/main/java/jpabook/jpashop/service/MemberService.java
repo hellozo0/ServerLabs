d5adf7209e7bc84e43c1b1350cb72af686792195
An HyeonJu <ahju0011@gmail.com>
2022-07-25 14:07:34 +0900

    [CHORE] 회원 수정 API

--- jpa_2_shop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpa_2_shop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -33,6 +33,14 @@ public class MemberService {
         }
     }
 
+     /**
+      *  회원 수정 */
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
+
     //회원 전체 조회
     public List<Member> findMembers() {
         return memberRepository.findAll();
