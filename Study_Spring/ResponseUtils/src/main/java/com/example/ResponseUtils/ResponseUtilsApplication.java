fc9d059fd00da4c84a1d9a6dca706c38e809d689
hellozo0 <ahju0011@gmail.com>
2023-10-09 22:05:46 +0900

    파일 세팅

---
+++ Study_Spring/ResponseUtils/src/main/java/com/example/ResponseUtils/ResponseUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResponseUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseUtilsApplication.class, args);
+	}
+
+}
